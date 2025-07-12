1ad50933b7a5b16967465023e4008e395cef1926
aryansaxena978 <aryansaxena978@gmail.com>
2025-07-12 15:08:48 +0530

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,12 @@
 // postcss.config.js
 import tailwindcss from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
 
 export default {
   plugins: [
     tailwindcss(),
-    autoprefixer,
+    autoprefixer(),
   ],
 }
+
 
